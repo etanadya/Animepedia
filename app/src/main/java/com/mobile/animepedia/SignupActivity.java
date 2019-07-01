97d891dac5c250ffb0a7371494a80f37bbcb0d95
iamjue <juandriwandi@yahoo.co.id>
2019-07-01 13:11:56 +0700

    Signup database success

--- app/src/main/java/com/mobile/animepedia/SignupActivity.java
+++ app/src/main/java/com/mobile/animepedia/SignupActivity.java
@@ -5,27 +5,51 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
-    private EditText edtEmailSignup, edtPasswordSignup;
+    EditText edtEmailSignup, edtPasswordSignup, edtNameSignup, edtTglLahir;
     private Button btnGotoLogin, btnSignup;
     private FirebaseAuth auth;
     private ProgressBar progressBar;
+    final String urlAdd = "http://192.168.43.18/animepedia/insert_user.php";
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+    int success;
+    String tag_json_obj = "json_obj_req";
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        textView = findViewById(R.id.tv);
+
+        edtNameSignup = findViewById(R.id.edt_name);
+        edtTglLahir = findViewById(R.id.edt_ttl);
         edtEmailSignup = findViewById(R.id.edt_emailSignup);
         edtPasswordSignup = findViewById(R.id.edt_passSignup);
         btnSignup = findViewById(R.id.btn_signup);
@@ -33,6 +57,8 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         btnGotoLogin = findViewById(R.id.btn_gotologin);
         btnGotoLogin.setOnClickListener(this);
         progressBar = findViewById(R.id.progressBar);
+
+
 
         auth = FirebaseAuth.getInstance();
 
@@ -45,6 +71,8 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
             case R.id.btn_signup:
                 Toast.makeText(this, "Ok", Toast.LENGTH_SHORT).show();
                 Signup();
+                Adduser();
+
                 break;
             case R.id.btn_gotologin:
                 Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
@@ -100,4 +128,67 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         super.onResume();
         progressBar.setVisibility(View.GONE);
     }
+
+    public void Adduser() {
+
+        final String nama = edtNameSignup.getText().toString().trim();
+        final String tgl_lahir = edtTglLahir.getText().toString().trim();
+        final String email = edtEmailSignup.getText().toString().trim();
+        final String pwd = edtPasswordSignup.getText().toString().trim();
+
+        StringRequest strReq = new StringRequest(Request.Method.POST, urlAdd, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Response: " + response.toString());
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    success = jObj.getInt(TAG_SUCCESS);
+
+                    // Cek error node pada json
+                    if (success == 1) {
+                        Log.d("Add/update", jObj.toString());
+
+
+                        Toast.makeText(SignupActivity.this, jObj.getString(TAG_MESSAGE), Toast.LENGTH_LONG).show();
+
+                    } else {
+                        Toast.makeText(SignupActivity.this, jObj.getString(TAG_MESSAGE), Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    // JSON error
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Error: " + error.getMessage());
+                Toast.makeText(SignupActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting parameters ke post url
+                Map<String, String> params = new HashMap<String, String>();
+                // jika id kosong maka simpan, jika id ada nilainya maka update
+
+                params.put("nama", nama);
+                params.put("tgl_lahir", tgl_lahir);
+                params.put("email", email);
+                params.put("pwd", pwd);
+
+
+                return params;
+            }
+
+        };
+
+        AppController.getInstance().addToRequestQueue(strReq, tag_json_obj);
+    }
+
 }
