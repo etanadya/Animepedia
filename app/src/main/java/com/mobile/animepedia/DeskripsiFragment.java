a372cc051fffe483c91400d7db20d4678715608f
etanadya <nadyanurarrosy@gmail.com>
2019-07-14 12:42:34 +0700

    terbaru justify

--- app/src/main/java/com/mobile/animepedia/DeskripsiFragment.java
+++ app/src/main/java/com/mobile/animepedia/DeskripsiFragment.java
@@ -1,19 +1,24 @@
 package com.mobile.animepedia;
 
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.codesgood.views.JustifiedTextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DeskripsiFragment extends Fragment {
-    TextView tvdes;
+    JustifiedTextView tvdes;
     public static DeskripsiFragment newInstance() {
         return new DeskripsiFragment();
     }
@@ -25,6 +30,7 @@ public class DeskripsiFragment extends Fragment {
 
 
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -32,7 +38,10 @@ public class DeskripsiFragment extends Fragment {
         View view = (View) inflater.inflate(R.layout.fragment_deskripsi, container, false);
         tvdes = view.findViewById(R.id.tv_deskripsi_frag);
 
+
+
         DetailActivity detailActivity = new DetailActivity();
+//        tvdes.setJustificationMode(Layout.JUSTIFICATION_MODE_INTER_WORD);
 
         tvdes.setText(detailActivity.EXTRA_DESKRIPSI);
         return view;
